775c209a7e1a8b0c7842221dd99bead3573f94fb
Ricardo Robayo <ricardo@DESKTOP-3OI2EQM.localdomain>
2023-11-05 11:42:24 -0500

    sc

---
+++ src/Pages/Products/index.jsx
@@ -0,0 +1,13 @@
+
+
+const Products = () => {
+    return (
+        <div>
+            <span>Product 1</span>
+            <span>Product 2</span>
+            <span>Product 3</span>
+        </div>
+    )
+}
+
+export default Products;
---
+++ src/Routes/App.jsx
@@ -0,0 +1,23 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from '../Pages/Home';
+import Products from '../Pages/Products';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="reactr/home" element={<Home />} />
+          <Route path="reactr/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
