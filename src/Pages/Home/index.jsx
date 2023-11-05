2cd0c4f2d42688693befaabaeeb023f6dae46c68
Ricardo Robayo <ricardo@DESKTOP-3OI2EQM.localdomain>
2023-11-05 15:58:43 -0500

    sc

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,19 @@
 
+import { NavLink } from 'react-router-dom'
+
+
 import React from 'react'
 
 const Home = () => {
     return (
         <div>
+            <ul>
+                <li>
+                    <NavLink to={'/reactr/products'}>Products</NavLink>
+                </li>
+            </ul>
             <h1>Home</h1>
+
         </div>
     )
 }
